8cf36f3c96993769dab7fb272a2ef7e61e45fe52
owezalty <mrt291949@gmail.com>
2022-12-30 01:00:34 +0000

    new commit

--- src/test/java/techproed/tests/day13/Day13_FirstDriverClass.java
+++ src/test/java/techproed/tests/day13/Day13_FirstDriverClass.java
@@ -1,6 +1,7 @@
 package techproed.tests.day13;
 
 import org.testng.annotations.Test;
+import techproed.utilities.ConfigurationReader;
 import techproed.utilities.Driver;
 
 public class Day13_FirstDriverClass {
@@ -8,8 +9,13 @@ public class Day13_FirstDriverClass {
     @Test
     public void firstDriverTest(){
         //driver =>>>> Driver.getDriver();
-        Driver.getDriver().get("https://www.amazon.com/");
-        Driver.getDriver().navigate().to("https://www.google.com/");
+//        Driver.getDriver().get("https://www.amazon.com/");
+//        Driver.getDriver().navigate().to("https://www.google.com/");
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("amazon_url"));
+        Driver.getDriver().navigate().to(ConfigurationReader.getProperty("google_url"));
+
+
         System.out.println("Google Title : " + Driver.getDriver().getTitle());
         Driver.closeDriver();
 
