f5a6c2813835c0901375eda5e6af98d3db109565
owezalty <mrt291949@gmail.com>
2022-12-30 01:27:08 +0000

    new commit

--- src/test/java/techproed/tests/day13/Day13_FirstDriverClass.java
+++ src/test/java/techproed/tests/day13/Day13_FirstDriverClass.java
@@ -12,6 +12,8 @@ public class Day13_FirstDriverClass {
 //        Driver.getDriver().get("https://www.amazon.com/");
 //        Driver.getDriver().navigate().to("https://www.google.com/");
 
+        //We are using configuration file to get the urls
+        //We do it because of reusability and making the code more dynamic
         Driver.getDriver().get(ConfigurationReader.getProperty("amazon_url"));
         Driver.getDriver().navigate().to(ConfigurationReader.getProperty("google_url"));
 
